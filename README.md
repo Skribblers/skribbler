660c3830d3e3fcf1e527221f8b69c7cf9ca1ec88
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2023-07-18 14:06:50 -0700

    add tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # skribbler
 A headless client that can join Skribbl.io lobbies.
+
+[Protocol Docs](https://gist.github.com/MrDiamond64/b2081f2cb4ca6d11e848edaeb5ae1814)
---
+++ test.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto");
+const { Client } = require("./index.js");
+
+const random = crypto.randomBytes(16).toString("hex");
+
+const client = new Client({
+	name: "Skribbler"
+});
+
+client.on("connected", () => {
+	console.log("Logged into lobby.");
+
+	client.sendMessage(random);
+});
+
+client.on("text", (data) => {
+	if(data.msg === random) {
+		console.log(`Successfully recieved random string!`);
+		process.exit(0);
+	}
+});
+
+client.on("disconnect", (reason) => {
+	console.error("Bot got kicked.");
+	console.error(reason);
+	process.exit(1);
+});
