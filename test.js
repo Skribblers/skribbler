4614c38b98c22491fb4d2447b13f0d3bb8d99d4c
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2023-12-29 18:27:49 -0600

    Improve tests
    Now tests proxy

--- test.js
+++ test.js
@@ -1,24 +1,41 @@
 const crypto = require("crypto");
-const { Client } = require("./index.js");
+const { Client, Proxy } = require("./index.js");
 
-const random = crypto.randomBytes(16).toString("hex");
+const random1 = crypto.randomBytes(16).toString("hex");
+const random2  = crypto.randomBytes(16).toString("hex");
+
+const proxy = new Proxy({
+	port: 5732
+});
+
+proxy.on("playerJoin", (player) => {
+	player.on("outgoing", (name, args) => {
+		if(args.id === 30) args.data += random2;
+	});
+});
 
 const client = new Client({
-	name: "Skribbler"
+	name: "Skribbler",
+	serverURL: "http://localhost:5732"
 });
 
 client.on("connect", () => {
 	console.log("Logged into lobby.");
 
-	client.sendMessage(random);
+	client.sendMessage(random1);
 });
 
 client.on("text", (data) => {
-	if(data.msg === random) {
+	if(data.msg === `${random1}${random2}`) {
 		console.log(`Successfully recieved random string!`);
 		process.exit(0);
 	}
 });
+
+setInterval(() => {
+	console.error(`Did not recieve "${random1}${random2}" after 10000ms`);
+	process.exit(1);
+}, 10000);
 
 client.on("disconnect", (reason) => {
 	console.error("Bot got kicked.");
