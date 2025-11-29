8652475b26bd2bb1f13ec2e8bb67fd153a0d64b0
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2025-11-29 14:20:17 -0600

    refactor: Clean example code

--- examples/joinPrivateLobby.js
+++ examples/joinPrivateLobby.js
@@ -15,10 +15,6 @@ client.on("text", (data) => {
 });
 
 client.on("stateUpdate", (data) => {
-	/**
-	 * @type {NodeJS.Timeout}
-	 */
-	let interval;
 	switch(data.state) {
 		// When given a chance to choose a word to draw, the bot will select the 2nd word
 		case Constants.GameState.USER_PICKING_WORD: {
@@ -31,13 +27,13 @@ client.on("stateUpdate", (data) => {
 
 		// Once the bot can draw, it will draw random stuff and then undo it
 		case Constants.GameState.CAN_DRAW: {
-			if(client.currentDrawer?.id !== client.userId) break;
+			if(!client.canvas.canDraw) break;
 
 			console.log("Drawing... now!");
 			client.canvas.draw([[0,1,32,108,82,108,82],[0,1,6,108,82,117,82]]);
 			client.canvas.draw([[0,1,32,546,393,588,373],[0,1,6,588,373,627,354],[0,1,6,627,354,648,345]]);
 
-			interval = setInterval(() => {
+			const interval = setInterval(() => {
 				// If there are no more commands left to undo then stop the interval
 				if(client.canvas.drawCommands.length === 0) return clearInterval(interval);
 
